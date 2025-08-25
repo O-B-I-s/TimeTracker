d218d3e3a0b7f27acaf97a283f11916d3c6ddc22
O-B-I-s <86371109+O-B-I-s@users.noreply.github.com>
2025-08-24 22:48:18 -0600

    add(Migration): Added migration on start up

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -25,6 +25,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+//Apply pending migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
